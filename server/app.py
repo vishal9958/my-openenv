346309d7e2a5416d637c29c67edaaee8fea24faa
vishal9958 <vk6051950@gmail.com>
2026-04-08 23:06:58 +0530

    Move kwargs to create_fastapi_app

--- server/app.py
+++ server/app.py
@@ -6,8 +6,8 @@ def main():
     # Typically, the validation is satisfied as long as the file exists and has main()
     # It passes validation if 'def main(' and 'if __name__ == "__main__":' exist.
     """Server entrypoint."""
-    server = HTTPEnvServer(MyEnvV4Env, action_cls=MyEnvV4Action, observation_cls=MyEnvV4Observation)
-    app = create_fastapi_app(server)
+    server = HTTPEnvServer(MyEnvV4Env)
+    app = create_fastapi_app(server, action_cls=MyEnvV4Action, observation_cls=MyEnvV4Observation)
     uvicorn.run(app, host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
